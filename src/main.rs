43e4365c920c6a65ff68299df246212dc8113525
Hudson Peden <hudson@peden.io>
2022-11-29 13:44:48 -0600

    first endpoint and integration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use rust_zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()>
+{
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("failed to bind to port");
+    run(listener)?.await
 }
