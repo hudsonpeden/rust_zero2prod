d8e1058a1f3f75d6ec200a007e4867b97c97d123
Hudson Peden <hudson@peden.io>
2022-12-07 14:11:41 -0600

    refactor into modules

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::net::TcpListener;
-
-use rust_zero2prod::run;
+use rust_zero2prod::startup;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()>
 {
     let listener = TcpListener::bind("127.0.0.1:8000")
         .expect("failed to bind to port");
-    run(listener)?.await
+    startup::run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use rust_zero2prod::startup::{run};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -22,8 +23,53 @@ fn spawn_app() -> String {
         .expect("Failed to bind to random port");
 
     let port = listener.local_addr().unwrap().port();
-    let server = rust_zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data(){
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let res = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute requeest");
+    assert_eq!(200, res.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(invalid_body)
+        .send()
+        .await
+        .expect("failed to execute req");
+
+        assert_eq!(
+            400, 
+            response.status().as_u16(),
+            "the api did not fail with 400 when the payload was {}",
+            error_message
+        )
+    }
 }
